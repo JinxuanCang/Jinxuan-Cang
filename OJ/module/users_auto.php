3e8837c8df6a6c138e0d8189f895f5853e88bc7c
Jinxuan Cang <jinxuancang@gmail.com>
2017-07-29 14:47:54 -0400

    .

--- OJ/module/users_auto.php
+++
@@ -1,8 +0,0 @@
-<script>
-	function AutoLogoff() {window.open("../login","_top");}
-</script>
-<?php if (!isset($_SESSION['username']) or $_SESSION['username']=="" or $_SESSION['username']==null) { ?>
-  <script>
-    AutoLogoff();
-  </script>
-<?php $_SESSION["logstatu"] = "No";} ?>
